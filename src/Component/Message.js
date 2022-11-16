f70b55da4a71435a029519b115252ac195e13d8d
SunWeb07 <sunnymawani786@gmail.com>
2022-11-16 23:41:31 +0530

    React assign

---
+++ src/Component/Message.js
@@ -0,0 +1,12 @@
+const Message=(props)=>{
+
+return(
+
+<div>
+<h4>Message from {this.name}</h4>
+<p>This is message to show how component works</p>
+</div>
+);
+
+}
+export default Message;
---
+++ src/HomeWork.js
@@ -0,0 +1,9 @@
+const HomeWork = ()=>{
+    return (
+        <div>
+            <h2>I'm also Component</h2>
+            <p>I can be functional or classful</p>
+        </div>
+    )
+}
+export default HomeWork;
